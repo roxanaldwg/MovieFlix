4648b8a4626005a39c3c1b2d1f02297142148ff8
roxanaldwg <roxana_ludwig@yahoo.de>
2022-01-09 15:37:13 +0100

    created "auth.js" file
    - created new “/login” endpoint for registered users that contains logic for authenticating users with basic HTTP authentication and generating a JWT token for authenticating future requests.

---
+++ auth.js
@@ -0,0 +1,37 @@
+const jwtSecret = 'your_jwt_secret'; // same key used in the JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
+
+require('./passport'); // passport file
+
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, // username encoded in the JWT
+    expiresIn: '7d', // token expiry
+    algorithm: 'HS256' // algorithm used to “sign” or encode the values of the JWT
+  });
+}
+
+
+/* POST login. */
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+}
